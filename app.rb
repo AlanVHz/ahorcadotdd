6a09486f86fb6d4a6545840ec0da23fe63aa942f
ALan <alanvhz.09@hotmail.com>
2017-01-18 10:50:41 -0600

    paginaMD

--- app.rb
+++ app.rb
@@ -3,13 +3,7 @@ require 'sinatra'
 enable :sessions
 
 get '/' do 
-	session['saludo'] = 'Que onda'
-
-	session['int'] = parametros[:campo1]
-	puts (session['int'])
-
-	puts "Viene del html"
 	erb :index
 end
 
-post '/intento'
+#post '/intento'
